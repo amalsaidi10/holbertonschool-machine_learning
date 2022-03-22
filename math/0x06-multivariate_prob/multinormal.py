11b8620db5dbbda873a86926c219f9443684f5c4
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-03-22 16:42:53 +0100

    Create multinormal.py

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Defines `mean_cov`."""
+import numpy as np
+
+
+class MultiNormal:
+    """A multivariate normal distribution."""
+
+    def __init__(self, data):
+        """
+        Initializes a MultiNormal.
+        data: A numpy.ndarray of shape (d, n) containing the data set:
+            d: The number of dimensions in each data point
+            n: The number of data points
+        """
+        if type(data) != np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        elif data.shape[1] < 2:
+            raise ValueError('data must contain multiple data points')
+
+        sample_count = data.shape[1]
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        mu = self.mean
+        self.cov = np.matmul(data - mu, data.T - mu.T) / (sample_count - 1)
+
+    def pdf(self, x):
+        """
+        Calculates the probability density at a data point.
+        x: A numpy.ndarray of shape (d, 1) containing the data point whose PDF
+            should be calculated:
+            d: The number of dimensions of the MultiNormal instance
+        Returns: The value of the PDF at a point.
+        """
+        if not type(x) is np.ndarray:
+            raise TypeError('x must be a numpy.ndarray')
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        d0, d1 = x.shape
+        if d0 != d or d1 != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        tau = np.pi * 2
+        determinant = np.linalg.det(self.cov)
+        x_centered = x - self.mean
+        probability_density = np.exp(
+                -0.5 *
+                x_centered.T @
+                np.linalg.inv(self.cov) @
+                x_centered
+            ) / np.sqrt(tau ** d * determinant)
+
+        return np.asscalar(probability_density)
