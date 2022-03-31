0dac5861d1a5f73e2d1334d10eea805479cd488f
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-03-31 11:36:43 +0100

    Update 0-initialize.py

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """initialize"""
 import numpy as np
+
 def initialize(X, k):
- """ initializes cluster centroids for K-means"""
- if type(X) is not np.ndarray or X.ndim != 2:
-      return None
- if type(k) is not int or k < 1:
-      return None
-d=X.shape[1]
-min=np.amin(X,axis=0)
-max=np.amax(X,axis=0)
-centroides=np.random.uniform(low=min,high=max,size=(k,d))
-return centroides
+  """ initializes cluster centroids for K-means"""
+  if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+  if type(k) is not int or k < 1:
+        return None
+  d=X.shape[1]
+  min=np.amin(X,axis=0)
+  max=np.amax(X,axis=0)
+  centroides=np.random.uniform(low=min,high=max,size=(k,d))
+  return centroides
