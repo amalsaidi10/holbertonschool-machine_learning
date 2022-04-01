880aa7cca5218c3c7f68db3f39aa1f3c4197c8da
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-01 12:08:24 +0100

    Create 4-initialize.py

---
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def initialize(X, k):
+    """initializes variables for a Gaussian Mixture Model"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None, None
+    if type(k) is not int or k < 1:
+        return None, None, None
+
+    _,  d = X.shape
+    pi = np.full(k, 1/k) # a new array of shape k filled with 1/k
+    m, _ = kmeans(X, k, iterations=100)
+    # S = np.array([np.eye(d) for i in range(k)])   
+    S = np.tile(np.eye(d), (k, 1, 1))
+    return pi, m, S
