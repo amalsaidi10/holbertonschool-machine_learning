b8d3e561cb6e7c57f76969c8ef3ec7769bdd27a9
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-06 14:12:05 +0100

    Update 0-markov_chain.py

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -6,14 +6,15 @@ def markov_chain(P, s, t=1):
     new_state
   """
   n= P.shape[0]
-  d=P.shape[1]
+  d= P.shape[1]
   if type(P) is not np.ndarray or P.ndim!=2 or n!= d :
     return None
   if not np.allclose(np.sum(P, axis=1), 1) :
         return None
-  if type(s) is not np.ndarray or s.shape[0]!=1 or s.shape[1]!=P.shape[0] :
+  if type(s) is not np.ndarray or s.shape[0]!= 1 or s.shape[1]!= P.shape[0] :
     return None
   if t < 1 or type(t) is not int :
     return None
-  product=np.linalg.matrix_power(P,t)
-  return np.matmul(s,product)
+  product=np.linalg.matrix_power(P , t)
+  return np.matmul(s , product)
+
