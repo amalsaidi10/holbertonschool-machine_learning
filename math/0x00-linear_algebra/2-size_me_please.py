9d2f4210770b97d3981641eb8d131f206a004859
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-14 13:40:59 +0100

    Create 2-size_me_please.py

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    shape=[]
+    while type(matrix) is list and len(matrix) > 0:
+        shape.append(len(matrix))
+        matrix=matrix[0]
+    return shape
