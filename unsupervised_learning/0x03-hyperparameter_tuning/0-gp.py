3ae61389891a9620498fd4b956a0da85b4756cb1
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-14 08:50:18 +0100

    Create 0-gp.py

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+
+class GaussianProcess:
+    def __init__(self, X, Y, l=1, sigma_f=1):
+        self.X = X
+        self.Y = Y
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X, X) #cov(x,x)
+
+    def kernel(self, X1, X2):
+      K = (self.sigma_f**2) * np.exp(np.square(X1.T -  X2) / -(2 * (self.l ** 2)))
+      return K 
