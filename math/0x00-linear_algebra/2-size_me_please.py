a8c51fe49b2c60fb52e1aae46987c375078d0761
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-14 13:51:51 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -2,11 +2,11 @@
 
 def matrix_shape(matrix):
   """ that calculates the shape of a matrix:
-
-You can assume all elements in the same dimension are of the same type/shape
-The shape should be returned as a list of integers"""
+      You can assume all elements in the same dimension are of the same type/shape
+      The shape should be returned as a list of integers
+  """
   shape = []
-  while type(matrix) is list and len(matrix) >0:
+  while type(matrix) is list and len(matrix) > 0:
       shape.append(len(matrix))
       matrix = matrix[0]
   return shape
