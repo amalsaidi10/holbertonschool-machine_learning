a19641d7741a853deb359fb570e0b20da58ec559
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-14 09:08:14 +0100

    Update 0-gp.py

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -11,6 +11,6 @@ class GaussianProcess:
         self.K = self.kernel(X, X)
 
     def kernel(self, X1, X2):
-        s=(self.sigma_f**2)
+        s=(self.sigma_f ** 2)
         K = s * np.exp(np.square(X1 - X2.T) / -(2 * (self.l ** 2)))
         return K
