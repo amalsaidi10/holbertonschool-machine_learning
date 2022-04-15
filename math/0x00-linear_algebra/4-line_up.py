5c6625454d2fb76dc16cb224cd070ffd251aa14c
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-15 14:10:35 +0100

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -4,11 +4,11 @@ adds two arrays element-wise
 """
 
 
-def add_arrays(arr1, arr2) :
+def add_arrays(arr1, arr2):
     """add array"""
     new_arr = []
-    if len(arr1) != len(arr2) :
-       return None
-    for i in range(len(arr1)) :
+    if len(arr1) != len(arr2):
+        return None
+    for i in range(len(arr1)):
         new_arr.append(arr1[i] + arr2[i])
     return new_arr
