faed97fa57bfedc5ff7a8ee4f34036018a715f9a
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-15 13:51:19 +0100

    Create 3-flip_me_over.py

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+  """ that calculates the shape of a matrix:
+
+You can assume all elements in the same dimension are of the same type/shape
+The shape should be returned as a list of integers"""
+  
+  
+  shape = []
+  while type(matrix) is list and len(matrix) > 0:
+      shape.append(len(matrix))
+      matrix=matrix[0]
+  return shape
