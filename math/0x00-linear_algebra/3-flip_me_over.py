ec7c704e58b2e40481efe4e76bff6bfb065343ff
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-15 13:58:41 +0100

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -3,7 +3,7 @@ def matrix_transpose(matrix):
     """returns the transpose of a 2d matrix"""
     transpose = []
     for i in range(len(matrix[0])):
-        transpose.append([]) 
+        transpose.append([])
         for j in range(len(matrix)):
             transpose[i].append(matrix[j][i])
     return transpose
