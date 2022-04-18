336bf9c37f50e09cfae68a124cc8b50f56dcf717
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-18 10:53:34 +0100

    Create 100-slice_like_a_ninja.py

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,30 @@
+
+        
+  def np_slice(matrix, axes={}):
+    """This returns a slice of an ndarray"""
+      for a in sorted(axes.keys()):
+
+          val = axes[a]
+          if len(val) == 3:
+              start = val[0]
+              stop = val[1]
+              step = val[2]
+          elif len(val) == 2:
+              start = val[0]
+              stop = val[1]
+              step = None
+          elif len(val) == 1:
+              start = None
+              stop = val[0]
+              step = None
+          else:
+              start = None
+              stop = None
+              step = None
+
+          s = slice(start, stop, step)
+          slc = [slice(None)] * len(matrix.shape)
+          slc[a] = s
+          matrix = matrix[tuple(slc)]
+
+     return matrix
