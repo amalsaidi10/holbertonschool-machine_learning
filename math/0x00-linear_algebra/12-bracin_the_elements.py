60fd9261728632262641002eaf869fe4909222fd
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-18 10:24:46 +0100

    Update 12-bracin_the_elements.py

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -4,9 +4,9 @@
 
 def np_elementwise(mat1, mat2):
   """ calculate different arithmetic operation of a matrix"""
-    add=mat1+mat2
-    sub=mat1-mat2
-    mul=mat1*mat2
-    div=mat1/mat2
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
     return add,sub,mul,div
   
