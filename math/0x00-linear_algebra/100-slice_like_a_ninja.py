f9e3c49ceff26df957cc15cc6f7d9a96ef04b7f0
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-18 10:58:54 +0100

    Update 100-slice_like_a_ninja.py

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
 
-        
-def np_slice(matrix, axes={}):
-"""This returns a slice of an ndarray"""
-        for a in sorted(axes.keys()):
+ def np_slice(matrix, axes={}):
+    """This returns a slice of an ndarray"""
+    for a in sorted(axes.keys()):
 
-          val = axes[a]
-          if len(val) == 3:
-              start = val[0]
-              stop = val[1]
-              step = val[2]
-          elif len(val) == 2:
-              start = val[0]
-              stop = val[1]
-              step = None
-          elif len(val) == 1:
-              start = None
-              stop = val[0]
-              step = None
-          else:
-              start = None
-              stop = None
-              step = None
+        val = axes[a]
+        if len(val) == 3:
+            start = val[0]
+            stop = val[1]
+            step = val[2]
+        elif len(val) == 2:
+            start = val[0]
+            stop = val[1]
+            step = None
+        elif len(val) == 1:
+            start = None
+            stop = val[0]
+            step = None
+        else:
+            start = None
+            stop = None
+            step = None
 
-          s = slice(start, stop, step)
-          slc = [slice(None)] * len(matrix.shape)
-          slc[a] = s
-          matrix = matrix[tuple(slc)]
+        s = slice(start, stop, step)
+        slc = [slice(None)] * len(matrix.shape)
+        slc[a] = s
+        matrix = matrix[tuple(slc)]
 
-        return matrix
+    return matrix
+       
+
