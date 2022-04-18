5bbabb8713d99553f994acfed29fb40168306071
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-18 09:20:18 +0100

    Create 6-howdy_partner.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+"""function to concatenate two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+  """concatenate 2 arrays"""
+  arr = arr1 + arr2
+  return arr
