d11aafbeadd8683d08758b156442484987ef7933
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-18 09:26:49 +0100

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,14 +6,14 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """concatenate mat1 and mat2 along a specific axis"""
     if axis > 1:
         return None
-    elif axis == 1:# add in row 
+    elif axis == 1:
         if len(mat1) != len(mat2) or not len(mat1):  
             return None
         mat = []
         for i in range(len(mat1)):
             mat.append([])
             mat[i] = mat1[i] + mat2[i]
-    else: #add in coloumn 
+    else:
         if not len(mat1) or not len(mat2) or len(mat1[0]) != len(mat2[0]):
             return None
         mat = []
