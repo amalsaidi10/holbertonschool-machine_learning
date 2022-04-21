6dee601f8ba686f54848984951f26f6731d3bc6f
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-21 14:44:27 +0100

    Update 17-integrate.py

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,11 +4,11 @@ integral
 """
 
 
-def poly_integral(poly, C = 0):
+def poly_integral(poly, C=0):
     """
     calculates the integral of a polynomial
     """
-    if type(poly) is not list or not len(poly)or type(C) is not int:
+    if type(poly) is not list or not len(poly) or type(C) is not int:
         return None
     integral = [C]
     if poly == [0]:
