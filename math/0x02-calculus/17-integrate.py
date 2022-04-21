816af59a6be5fb2f65929cafa26221e65f5b7ac1
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-04-21 14:41:42 +0100

    Update 17-integrate.py

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,19 +5,19 @@ integral
 
 
 def poly_integral(poly, C=0):
-  """
-   calculates the integral of a polynomial
-  """
-  if type(poly) is not list or not len(poly)or type(C) is not int:
-      return None
-  integral = [C]
-  if poly == [0]:
-      return integral
-  for i, val in enumerate(poly):
-      result = val / (i + 1)
-      if int(result) == result :
-          result=int(result)
-      else:
-          result
-      integral.append(result)
-  return integral
+    """
+    calculates the integral of a polynomial
+    """
+    if type(poly) is not list or not len(poly)or type(C) is not int:
+        return None
+    integral = [C]
+    if poly == [0]:
+        return integral
+    for i, val in enumerate(poly):
+        result = val / (i + 1)
+        if int(result) == result :
+            result=int(result)
+        else:
+            result
+        integral.append(result)
+    return integral
