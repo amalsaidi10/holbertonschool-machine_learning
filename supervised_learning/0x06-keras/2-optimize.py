0aa8fb552b9a71233bcb025161719a4211738643
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-05-31 16:13:15 +0100

    Update 2-optimize.py

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-
 import tensorflow.keras as K
+"""
+write a function that optimize the model 
+"""
+
 
 def optimize_model(network, alpha, beta1, beta2):
-    network.compile(optimizer=K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2),
-                    loss='categorical_crossentropy', metrics=['accuracy'])
+    """
+    optimize the model 
+    """
+    network.compile(optimizer = K.optimizers.Adam(lr = alpha, beta_1 = beta1, beta_2 = beta2),
+                    loss = 'categorical_crossentropy', metrics = ['accuracy'])
   
