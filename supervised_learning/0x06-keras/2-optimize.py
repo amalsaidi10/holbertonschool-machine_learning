579d181b9952edf48e146fa70cd3c7a977ea07c4
amal saidi <85562282+amalsaidi10@users.noreply.github.com>
2022-05-31 16:08:41 +0100

    Update 2-optimize.py

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import keras
 
 
@@ -10,5 +11,5 @@ def optimize_model(network, alpha, beta1, beta2):
   Returns: None
   """
   opt =tf.keras.optimizers.Adam(learning_rate=alpha,beta_1=beta1,beta_2=beta2)
-  network.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'])
+  network.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
   
